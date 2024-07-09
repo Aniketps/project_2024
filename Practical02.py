40f254fc790fd3398759098b86d1de5cf6832a01
Aniketps <aniketp2327@gmail.com>
2024-07-09 15:30:52 +0530

    File added

---
+++ Practical02.py
@@ -0,0 +1,30 @@
+Data = {
+    "A" : ["B", "E"],
+    "B" : ["C", "G"],
+    "C" : [],
+    "D" : ["E"],
+    "E" : ["A"],   
+    "G" : ["D"]
+}
+
+# f(n) = g(n) + h(n)
+
+h_data = {
+    "A" : 11,
+    "B" : 6,
+    "C" : 99,
+    "D" : 1,
+    "E" : 7,
+    "G" : 0
+}
+
+def f(n):
+    result = g(n) + h(n)
+    return(result)
+
+# 
+def g(n):
+    
+    
+def h(n):
+    
